2a1f44803069912aa09fbca886de7dc03a1b5927
saaadksks-ux <saaadksks@gmail.com>
2026-09-13 22:31:45 +0500

    beggining done with controllers

--- src/DevQuestion.Web/Program.cs
+++ src/DevQuestion.Web/Program.cs
@@ -1,13 +1,14 @@
-var builder = WebApplication.CreateBuilder(args);
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 
-var app = builder.Build();
+WebApplication app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwaggerUI(option => option.SwaggerEndpoint("/openapi/v1.json", "DevQuestions"));
 }
 
 app.MapControllers();
