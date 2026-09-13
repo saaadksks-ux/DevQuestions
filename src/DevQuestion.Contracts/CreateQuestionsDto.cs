dafc3e99cc9da86bcc071caf18550e2854cad334
saaadksks-ux <saaadksks@gmail.com>
2026-09-13 22:54:38 +0500

    beggining done with controllers

---
+++ src/DevQuestion.Contracts/CreateQuestionsDto.cs
@@ -0,0 +1,8 @@
+﻿namespace DevQuestion.Contracts;
+
+public record CreatedQuestionDto(string Title, String Body, Guid UserId, Guid[] TagIds);
+
+public record GetQuestionsDto(string Search, Guid[] TagIds, int PageSize, int Limit);
+
+public record UpdateQuestionsDto(string Title, String Body, Guid[] TagIds);
+public record AddAnswerDto(Guid UserId, string Test);
