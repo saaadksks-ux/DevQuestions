096315758055e4dda1b27e7d2f759b5334d91fd3
saaadksks-ux <saaadksks@gmail.com>
2026-09-15 11:46:42 +0500

    Update

--- src/DevQuestion.Contracts/CreateQuestionsDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace DevQuestion.Contracts;
-
-public record CreatedQuestionDto(string Title, String Body, Guid UserId, Guid[] TagIds);
-
-public record GetQuestionsDto(string Search, Guid[] TagIds, int PageSize, int Limit);
-
-public record UpdateQuestionsDto(string Title, String Body, Guid[] TagIds);
-public record AddAnswerDto(Guid UserId, string Test);
